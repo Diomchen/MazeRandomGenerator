6ee8bfd1517240253906ceaa2b15b2277697fd4c
DiomChen <Diomchen@users.noreply.github.com>
2018-11-11 20:42:11 +0800

    迷宫递归——非递归广度优先遍历(BFS)+RandomQueue2.0+迷雾特效完成迷宫自动生成

--- src/RandomQueue.java
+++ src/RandomQueue.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class RandomQueue<E> {
-    private ArrayList<E> randomQueue;
+//    private ArrayList<E> randomQueue;
+    private LinkedList<E> randomQueue;
 
     public RandomQueue(){
-        randomQueue = new ArrayList<>();
+        randomQueue = new LinkedList<>();
     }
 
+    //初代随机队列---添加函数
+//    public void add(E e){
+//        randomQueue.add(e);
+//    }
+
+    //次代随机队列---添加函数
     public void add(E e){
-        randomQueue.add(e);
+        if(Math.random() <0.5){
+            randomQueue.addFirst(e);
+        }
+        else{
+            randomQueue.addLast(e);
+        }
     }
+
     public int size(){
         return randomQueue.size();
     }
@@ -18,16 +32,31 @@ public class RandomQueue<E> {
         return randomQueue.isEmpty();
     }
 
+    //初代随机队列---删除函数
+//    public E remove(){
+//        if(randomQueue.size() == 0){
+//            throw new IllegalArgumentException("randomQueue don't have element!");
+//        }
+//
+//        int randomIndex = (int)(Math.random()*randomQueue.size());
+//        E element = randomQueue.get(randomIndex);
+//        randomQueue.set(randomIndex,randomQueue.get(randomQueue.size()-1));
+//
+//        return element;
+//    }
+
+    //次代随机队列---删除函数
     public E remove(){
         if(randomQueue.size() == 0){
             throw new IllegalArgumentException("randomQueue don't have element!");
         }
-
-        int randomIndex = (int)(Math.random()*randomQueue.size());
-        E element = randomQueue.get(randomIndex);
-        randomQueue.set(randomIndex,randomQueue.get(randomQueue.size()-1));
-
+        E element;
+        if(Math.random()<0.5){
+            element = randomQueue.removeFirst();
+        }
+        else{
+            element = randomQueue.removeLast();
+        }
         return element;
     }
-
 }
